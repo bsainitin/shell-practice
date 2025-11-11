268d7ddc9a0391e32f908c7f757bd23d2f36780e
bsainitin <b.sainitin@gmail.com>
2025-11-11 14:04:06 +0530

    Basic defining and using Variables

---
+++ 03-variables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Defining a variable
+# variable_name=value
+
+name="Nitin"
+
+#No spaces around =.
+#Value can be text, numbers, etc
+
+#Using a variable
+#Use $ before the variable name:
+
+echo "Hello, $name!"
+
+
+#Example:
+
+name="Sai"
+age=25
+
+echo "My name is $name and"
+echo "I am $age years old"
+
