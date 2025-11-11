06aaaf81f294abaf90d3747aafc989673a34f1a4
bsainitin <b.sainitin@gmail.com>
2025-11-11 13:39:56 +0530

    Purpose of Shebang in shell scripts

---
+++ 01-shebang.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Shebang       ->  #!
+
+Interpreter   ->  /bin/bash
+
+The first line in a shell script should usually start with this Shebang line  ->  #!/bin/bash
+
+#To run the shell script we have to mention Shebang line on top top of the file like this file.
