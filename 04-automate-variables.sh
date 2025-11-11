c702f1a6165d87f3bd88ceeb88e4a34419f4d6ae
bsainitin <b.sainitin@gmail.com>
2025-11-11 16:49:04 +0530

    Automate Variables using $1, $2

---
+++ 04-automate-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1:: Are you ready to negotiate, $Person2"
+echo "$Person2:: Yes $Person1, i will but i have some conditions"
+echo "$Person1:: Ok, What are the conditions $Person2"
+echo "$Person2:: Stop mocking India with you Tariffs $Person1"
