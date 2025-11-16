f58aab847d6c62439b3815e17e425eeb88c219b1
bsainitin <b.sainitin@gmail.com>
2025-11-16 18:50:39 +0530

    Delete log files older than 14 Days

---
+++ 21-delete-old-logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LOG_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOG_FOLDER
+echo "Script started execution at $(date)" | tee -a $LOG_FILE
+
+SOURCE_DIR=/home/ec2-user/app-logs
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo "ERROR: $SOURCE_DIR does not exist"
+fi
+
+FILES_TO_DELETE=$(find $SCRIPT_DIR -name "*.log" -type f -mtime +14)
+
+while IFS= read -r filepath
+do 
+    echo "Deleting the file: $filepath"
+    rm -rf $filepath
+    echo "Deleted the file: $filepath"
+done <<< $FILES_TO_DELETE
