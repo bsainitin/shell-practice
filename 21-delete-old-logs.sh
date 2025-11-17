ed189ea56220f6ea34c533052aa5e7fe3367695e
bsainitin <b.sainitin@gmail.com>
2025-11-17 14:50:26 +0530

    Solved Error

--- 21-delete-old-logs.sh
+++ 21-delete-old-logs.sh
@@ -11,9 +11,10 @@ SOURCE_DIR=/home/ec2-user/app-logs
 
 if [ ! -d $SOURCE_DIR ]; then
     echo "ERROR: $SOURCE_DIR does not exist"
+    exit 1
 fi
 
-FILES_TO_DELETE=$(find $SCRIPT_DIR -name "*.log" -type f -mtime +14)
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
 
 while IFS= read -r filepath
 do 
