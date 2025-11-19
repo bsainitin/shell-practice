552a9e78a00aa39fffb98227a886e7d0893760d7
bsainitin <b.sainitin@gmail.com>
2025-11-19 15:54:44 +0530

    Modify script to run as command

--- 23-backup.sh
+++ 23-backup.sh
@@ -7,7 +7,8 @@ N="\e[0m"
 
 LOG_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+#LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOG_FOLDER/backup.log"
 SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} #If not provided considered as 14 days
